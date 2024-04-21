bc134d1f70c377b041b8efe25f9fa2780206a1da
Watcharapong Kaewphila <watcharapongmufo27@gmail.com>
2024-04-21 19:53:07 +0700

    update: routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,41 +1,81 @@
-import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 import { NotfoundComponent } from './demo/components/notfound/notfound.component';
 import { AppLayoutComponent } from "./layout/app.layout.component";
 import { AdminLayoutsComponent } from './shared/layouts/admin-layouts/admin-layouts.component';
 import { EmpLayoutsComponent } from './shared/layouts/emp-layouts/emp-layouts.component';
 
+const routes: Routes = [
+    {
+        path: '',
+        component: AppLayoutComponent,
+        children: [
+            {
+                path: '',
+                loadChildren: () => import('./demo/components/dashboard/dashboard.module').then(m => m.DashboardModule)
+            },
+            {
+                path: 'uikit',
+                loadChildren: () => import('./demo/components/uikit/uikit.module').then(m => m.UIkitModule)
+            },
+            {
+                path: 'utilities',
+                loadChildren: () => import('./demo/components/utilities/utilities.module').then(m => m.UtilitiesModule)
+            },
+            {
+                path: 'documentation',
+                loadChildren: () => import('./demo/components/documentation/documentation.module').then(m => m.DocumentationModule)
+            },
+            {
+                path: 'blocks',
+                loadChildren: () => import('./demo/components/primeblocks/primeblocks.module').then(m => m.PrimeBlocksModule)
+            },
+            {
+                path: 'pages',
+                loadChildren: () => import('./demo/components/pages/pages.module').then(m => m.PagesModule)
+            }
+        ]
+    },
+    {
+        path: 'admin',
+        component: AdminLayoutsComponent,
+        children: [
+            {
+                path: '',
+                loadChildren: () => import('./pages/admin/admin.module').then(m => m.AdminModule)
+            },
+        ]
+    },
+    {
+        path: 'emp',
+        component: EmpLayoutsComponent,
+        children: [
+            {
+                path: '',
+                loadChildren: () => import('./pages/emp/emp.module').then(m => m.EmpModule)
+            },
+        ]
+    },
+    {
+        path: 'auth',
+        loadChildren: () => import('./demo/components/auth/auth.module').then(m => m.AuthModule)
+    },
+    {
+        path: 'landing',
+        loadChildren: () => import('./demo/components/landing/landing.module').then(m => m.LandingModule)
+    },
+    {
+        path: 'notfound',
+        component: NotfoundComponent
+    },
+    {
+        path: '**', redirectTo: '/notfound'
+    },
+];
+
 @NgModule({
     imports: [
-        RouterModule.forRoot([
-            {
-                path: '', component: AppLayoutComponent,
-                children: [
-                    { path: '', loadChildren: () => import('./demo/components/dashboard/dashboard.module').then(m => m.DashboardModule) },
-                    { path: 'uikit', loadChildren: () => import('./demo/components/uikit/uikit.module').then(m => m.UIkitModule) },
-                    { path: 'utilities', loadChildren: () => import('./demo/components/utilities/utilities.module').then(m => m.UtilitiesModule) },
-                    { path: 'documentation', loadChildren: () => import('./demo/components/documentation/documentation.module').then(m => m.DocumentationModule) },
-                    { path: 'blocks', loadChildren: () => import('./demo/components/primeblocks/primeblocks.module').then(m => m.PrimeBlocksModule) },
-                    { path: 'pages', loadChildren: () => import('./demo/components/pages/pages.module').then(m => m.PagesModule) }
-                ]
-            },
-            {
-                path: 'admin', component: AdminLayoutsComponent,
-                children: [
-                    { path: 'admin', loadChildren: () => import('./pages/admin/admin.module').then(m => m.AdminModule) },
-                ]
-            },
-            {
-                path: 'emp', component: EmpLayoutsComponent,
-                children: [
-                    { path: 'emp', loadChildren: () => import('./pages/emp/emp.module').then(m => m.EmpModule) },
-                ]
-            },
-            { path: 'auth', loadChildren: () => import('./demo/components/auth/auth.module').then(m => m.AuthModule) },
-            { path: 'landing', loadChildren: () => import('./demo/components/landing/landing.module').then(m => m.LandingModule) },
-            { path: 'notfound', component: NotfoundComponent },
-            { path: '**', redirectTo: '/notfound' },
-        ], { scrollPositionRestoration: 'enabled', anchorScrolling: 'enabled', onSameUrlNavigation: 'reload' })
+        RouterModule.forRoot(routes, { scrollPositionRestoration: 'enabled', anchorScrolling: 'enabled', onSameUrlNavigation: 'reload' })
     ],
     exports: [RouterModule]
 })
--- src/app/pages/admin/admin-routing.module.ts
+++ src/app/pages/admin/admin-routing.module.ts
@@ -1,15 +1,27 @@
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProfileComponent } from './profile/profile.component';
 
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full'
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent
+  }
 
+];
 
 @NgModule({
-  imports: [RouterModule.forChild([
-    { path: 'dashboard', component: DashboardComponent },
-    { path: 'profile', component: DashboardComponent }
-
-  ])],
+  imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
 export class AdminRoutingModule { }
